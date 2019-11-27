74e2d022612207e604f265d9bd2cd5eab464a30c
Ajay Yadav <ajya@microsoft.com>
2019-11-27 15:46:38 +0530

    test

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -4,7 +4,7 @@ import {fetchFeatureFlags} from './FeatureFlag'
 async function run() {
   try {
     const actionType = core.getInput('action-type');
-    const projectId = core.getInput('projectId');
+    const projectId = core.getInput('project-id');
     if (actionType != 'GET-FEATURE-FLAGS') {
         // the Octocat doesn't want to be greeted here!
         throw new Error("Operation not supported");
