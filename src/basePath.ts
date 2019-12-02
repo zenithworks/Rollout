d388a3cd85f82890179b8d2ff295dab5854fac7e
Ajay Kumar Yadav <ajya@microsoft.com>
2019-12-02 14:41:00 +0530

    Update basePath.ts

--- src/basePath.ts
+++ src/basePath.ts
@@ -12,7 +12,7 @@ export const endActiveCall = () => ({
     type: "END_ACTIVECALL",
 });
 
-export const SET_FEATURE_FLAG_CONDITION: string = "SET_CONDITION";
+export const SET_FEATURE_FLAG_CONDITION: string = "set_condition";
 export const TOGGLE_FEATURE_FLAG: string = "TOGGLE";
 export const TURN_ON_FEATURE_FLAG = "TURN_ON";
 
